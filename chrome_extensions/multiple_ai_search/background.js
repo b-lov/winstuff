34c5354bf5f9ac44e915dbfeb6dc0ff0eff8b377
b-lov <max@bilov.de>
2024-09-29 16:56:19 +0200

    some chrome stuff

--- chrome_extensions/multiple_ai_search/background.js
+++ chrome_extensions/multiple_ai_search/background.js
@@ -1,11 +1,7 @@
 chrome.omnibox.onInputEntered.addListener((text) => {
-	const chatgptUrl = `https://chatgpt.com/?model=gpt-4o&q=${encodeURIComponent(
-		text
-	)}`;
+	const chatgptUrl = `https://chatgpt.com/?model=gpt-4o&q=${encodeURIComponent(text)}`;
 	const claudeUrl = `https://claude.ai/new?q=${encodeURIComponent(text)}`;
-	const perplexityUrl = `https://perplexity.ai/search?q=${encodeURIComponent(
-		text
-	)}`;
+	const perplexityUrl = `https://perplexity.ai/search?q=${encodeURIComponent(text)}`;
 
 	chrome.tabs.update({ url: chatgptUrl });
 	chrome.tabs.create({ url: claudeUrl });
