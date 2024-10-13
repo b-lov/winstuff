bf096b960e7e95c83dcfedbecbbf39de027d5d8b
b-lov <max@bilov.de>
2024-10-13 22:43:59 +0200

    add symlink powershell command

--- win/ps_scripts/useful_ps_commands.ps1
+++ win/ps_scripts/useful_ps_commands.ps1
@@ -1,2 +1,5 @@
 # download torrent into Downloads with no seeding
 aria2c --dir="$env:USERPROFILE\Downloads" --seed-time=0 "magnet:?xt=urn:btih:HASH"
+
+# create symbolic link (run as admin)
+New-Item -ItemType SymbolicLink -Path "C:\Users\YourUsername\Desktop\MyLink.txt" -Target "C:\OriginalFolder\OriginalFile.txt"
